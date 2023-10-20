d3b6ee88cc871c8d5bf4845e88983457c2618c63
Albert Lee <lee4382@purdue.edu>
2023-10-20 16:17:23 -0400

    Documentation of gdp_small_batch.py

--- gdp/small_batch/gdp_small_batch.py
+++ gdp/small_batch/gdp_small_batch.py
@@ -1,3 +1,11 @@
+"""
+gdp_small_batch.py
+The gdp_small_batch.py module contains the GDP model for the small batch problem based on the Kocis and Grossmann (1988) paper.
+The problem is based on the Example 4 of the paper.
+
+References:
+    - Kocis, G. R.; Grossmann, I. E. Global Optimization of Nonconvex Mixed-Integer Nonlinear Programming (MINLP) Problems in Process Synthesis. Ind. Eng. Chem. Res. 1988, 27 (8), 1407–1421. 
+"""
 import os
 
 import pyomo.environ as pe
@@ -9,99 +17,200 @@ from pyomo.opt.base.solvers import SolverFactory
 
 
 def build_small_batch():
+    """
+    The function build the GDP model for the small batch problem.
+
+    Args:
+        None
+    
+    Returns:
+        m: pyomo.ConcreteModel
+    """
     NK = 3
 
     # Model
     m = pe.ConcreteModel()
 
     # Sets
-    m.i = pe.Set(initialize=['a', 'b'])  # Set of products
+    m.i = pe.Set(initialize=['a', 'b'], doc='Set of produncts')  # Set of products, i=1,2
     m.j = pe.Set(initialize=['mixer', 'reactor',
-                             'centrifuge'])  # Set of stages
+                             'centrifuge'])  # Set of stages, j=1,2,3
     m.k = pe.RangeSet(NK)    # Set of potential number of parallel units
 
     # Parameters and Scalars
 
-    m.h = pe.Param(initialize=6000)  # Horizon time  (available time hrs)
-    m.vlow = pe.Param(initialize=250)  # Lower bound for size of batch unit
-    m.vupp = pe.Param(initialize=2500)  # Upper bound for size of batch unit
+    m.h = pe.Param(initialize=6000, doc='Horizon time [hr]')  # Horizon time  (available time) [hr]
+    m.vlow = pe.Param(initialize=250, doc='Lower bound for size of batch unit [L]')  # Lower bound for size of batch unit [L]
+    m.vupp = pe.Param(initialize=2500, doc='Upper bound for size of batch unit [L]')  # Upper bound for size of batch unit [L]
 
     # Demand of product i
-    m.q = pe.Param(m.i, initialize={'a': 200000, 'b': 150000})
+    m.q = pe.Param(m.i, initialize={'a': 200000, 'b': 150000}, doc='Production rate pf the product [kg/]')
     # Cost coefficient for batch units
     m.alpha = pe.Param(
-        m.j, initialize={'mixer': 250, 'reactor': 500, 'centrifuge': 340})
+        m.j, initialize={'mixer': 250, 'reactor': 500, 'centrifuge': 340}, doc='Cost coefficient for batch units')
     # Cost exponent for batch units
     m.beta = pe.Param(
-        m.j, initialize={'mixer': 0.6, 'reactor': 0.6, 'centrifuge': 0.6})
+        m.j, initialize={'mixer': 0.6, 'reactor': 0.6, 'centrifuge': 0.6}, doc='Cost exponent for batch units')
 
     def coeff_init(m, k):
+        """
+        Coefficient for number of parallel units.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            k (int)
+
+        Returns:
+            Coefficient for number of parallel units.
+        """
         return pe.log(k)
 
     # Represent number of parallel units
-    m.coeff = pe.Param(m.k, initialize=coeff_init)
+    m.coeff = pe.Param(m.k, initialize=coeff_init, doc='Coefficient for number of parallel units')
 
     s_init = {('a', 'mixer'): 2, ('a', 'reactor'): 3, ('a', 'centrifuge'): 4,
               ('b', 'mixer'): 4, ('b', 'reactor'): 6, ('b', 'centrifuge'): 3}
 
-    # Size factor for product i in stage j (kg per l)
-    m.s = pe.Param(m.i, m.j, initialize=s_init)
+    # Size factor for product i in stage j [Kg/L]
+    m.s = pe.Param(m.i, m.j, initialize=s_init, doc='Size factor for product i in stage j [Kg/L]')
 
     t_init = {('a', 'mixer'): 8, ('a', 'reactor'): 20, ('a', 'centrifuge'): 4,
               ('b', 'mixer'): 10, ('b', 'reactor'): 12, ('b', 'centrifuge'): 3}
 
-    # Processing time of product i in batch j   (hrs)
-    m.t = pe.Param(m.i, m.j, initialize=t_init)
+    # Processing time of product i in batch j [hr]
+    m.t = pe.Param(m.i, m.j, initialize=t_init, doc='Processing time of product i in batch j [hr]')
 
     # Variables
-    m.Y = pe.BooleanVar(m.k, m.j)    # Stage existence
+    m.Y = pe.BooleanVar(m.k, m.j, doc='Stage existence')    # Stage existence
     m.coeffval = pe.Var(m.k, m.j,  within=pe.NonNegativeReals,
-                        bounds=(0, pe.log(NK)))  # Activation of coeff
+                        bounds=(0, pe.log(NK)), doc='Activation of Coefficient')  # Activation of coeff
     m.v = pe.Var(m.j, within=pe.NonNegativeReals, bounds=(
-        pe.log(m.vlow), pe.log(m.vupp)))  # Volume of stage j
-    m.b = pe.Var(m.i, within=pe.NonNegativeReals)  # Batch size of product i
-    m.tl = pe.Var(m.i, within=pe.NonNegativeReals)  # Cycle time of product i
+        pe.log(m.vlow), pe.log(m.vupp)), doc='Colume of stage j [L]')  # Volume of stage j [L]
+    m.b = pe.Var(m.i, within=pe.NonNegativeReals, doc='Batch size of product i [L]')  # Batch size of product i [L]
+    m.tl = pe.Var(m.i, within=pe.NonNegativeReals, doc='Cycle time of product i [hr]')  # Cycle time of product i [hr]
     # Number of units in parallel stage j
-    m.n = pe.Var(m.j, within=pe.NonNegativeReals)
+    m.n = pe.Var(m.j, within=pe.NonNegativeReals, doc='Number of units in parallel stage j')
 
     # Constraints
 
     # Volume requirement in stage j
     @m.Constraint(m.i, m.j)
     def vol(m, i, j):
+        """
+        Volume Requirement for Stage j.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            i (str): product
+            j (str): stage
+        
+        Returns:
+            Constraint
+        """
         return m.v[j] >= pe.log(m.s[i, j]) + m.b[i]
 
     # Cycle time for each product i
     @m.Constraint(m.i, m.j)
     def cycle(m, i, j):
+        """
+        Cycle time for each product i.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            i (str): product
+            j (str): stage
+
+        Returns:
+            Constraint
+        """
         return m.n[j] + m.tl[i] >= pe.log(m.t[i, j])
 
     # Constraint for production time
     @m.Constraint()
     def time(m):
+        """
+        Production time constraint.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+
+        Returns:
+            Constraint
+        """
         return sum(m.q[i]*pe.exp(m.tl[i]-m.b[i]) for i in m.i) <= m.h
 
     # Relating number of units to 0-1 variables
     @m.Constraint(m.j)
     def units(m, j):
+        """
+        Relating number of units to 0-1 variables.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            j (str): stage
+        
+        Returns:
+            Constraint
+        """
         return m.n[j] == sum(m.coeffval[k, j] for k in m.k)
 
     # Only one choice for parallel units is feasible
     @m.LogicalConstraint(m.j)
     def lim(m, j):
+        """
+        Only one choice for parallel units is feasible.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            j (str): stage
+        
+        Returns:
+            Logical Constraint
+        """
         return pe.exactly(1, m.Y[1, j], m.Y[2, j], m.Y[3, j])
 
     # _______ Disjunction_________
 
     def build_existence_equations(disjunct, k, j):
+        """
+        Build the Logic Proposition (euqations) for the existence of the stage.
+
+        Args:
+            disjunct (pyomo.gdp.Disjunct): Disjunct block
+            k (int): number of parallel units
+            j (str): stage
+
+        Returns:
+            None, the proposition is built inside the function 
+        """
         m = disjunct.model()
 
         # Coeffval activation
         @disjunct.Constraint()
         def coeffval_act(disjunct):
+            """
+            Coeffval activation.
+
+            Args:
+                disjunct (pyomo.gdp.Disjunct): Disjunct block
+
+            Returns:
+                Constraint
+            """
             return m.coeffval[k, j] == m.coeff[k]
 
     def build_not_existence_equations(disjunct, k, j):
+        """
+        Build the Logic Proposition (euqations) for the unexistence of the stage.
+
+        Args:
+            disjunct (pyomo.gdp.Disjunct): Disjunct block
+            k (int): number of parallel units
+            j (str): stage
+
+        Returns:
+            None, the proposition is built inside the function
+        """
         m = disjunct.model()
 
         # Coeffval deactivation
@@ -117,6 +226,17 @@ def build_small_batch():
 
     @m.Disjunction(m.k, m.j)
     def Y_exists_or_not(m, k, j):
+        """
+        Build the Logical Disjunctions of the GDP model for the small batch problem.
+
+        Args:
+            m (pyomo.ConcreteModel): small batch GDP model
+            k (int): number of parallel units
+            j (str): stage
+        
+        Returns:
+            Disjunction
+        """
         return [m.Y_exists[k, j], m.Y_not_exists[k, j]]
 
     # Associate Boolean variables with with disjunction
@@ -128,6 +248,15 @@ def build_small_batch():
 
     # Objective
     def obj_rule(m):
+        """
+        Objective: mininimize the investment cost.
+        
+        Args:
+            m: pyomo.ConcreteModel
+        
+        Returns:
+            Objective function
+        """
         return sum(m.alpha[j]*(pe.exp(m.n[j] + m.beta[j]*m.v[j])) for j in m.j)
 
     m.obj = pe.Objective(rule=obj_rule, sense=pe.minimize)
@@ -166,7 +295,19 @@ def external_ref(m, x, logic_expr=None):
 
 
 def solve_with_minlp(m, transformation='bigm', minlp='baron', timelimit=10):
+    """
+    Solve the GDP optimization problem with a MINLP solver.
+    The function applies the big-M Reformulation on the GDP and solve the MINLP problem with BARON.
 
+    Args:
+        m (pyomo.ConcreteModel): GDP optimization model
+        transformation (str, optional): Reformulation applied to the GDP.
+        minlp (str, optional): MINLP solver.
+        timelimit (float, optional): Time limit for the MINLP solver.
+
+    Returns:
+        m (pyomo.ConcreteModel): GDP optimization model with the solution.
+    """
     # Transformation step
     pe.TransformationFactory('core.logical_to_linear').apply_to(m)
     transformation_string = 'gdp.' + transformation
@@ -204,7 +345,7 @@ def solve_with_minlp(m, transformation='bigm', minlp='baron', timelimit=10):
 
 if __name__ == "__main__":
     m = build_small_batch()
-    #m_solved = solve_with_minlp(m, transformation='bigm', minlp='baron', timelimit=120)
+    # m_solved = solve_with_minlp(m, transformation='bigm', minlp='baron', timelimit=120)
 
     # EXTERNAL REF TEST (this thest can be deleted)
     newmodel = external_ref(m, [1, 2, 3], logic_expr=None)
